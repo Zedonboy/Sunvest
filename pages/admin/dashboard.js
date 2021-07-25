3a36ddf042e5775d21ba364dcf61208ead092542
zedonboy <zedonbiz@gmail.com>
2021-07-25 17:23:16 +0100

    update

--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -1,22 +1,22 @@
-import Table from "../../components/admin/Table";
+// import Table from "../../components/admin/Table";
 
-/* eslint-disable @next/next/no-img-element */
-export default function Dashboard() {
-  return (
-    <main className="flex">
-      <aside className="h-screen w-1/6 bg-purple-100">
-        <ul className="flex flex-col text-purple-500">
-          <li className="p-4"><img src="/images/logo.png"/></li>
-          <li className="p-4 hover:bg-purple-600 hover:text-purple-100"><a href="">Projects</a></li>
-          <li className="p-4"><a href="">Investors</a></li>
-        </ul>
-      </aside>
-      <section className="h-screen p-12 w-5/6 flex space-y-3 flex-col">
-        <section className="flex justify-end">
-          <button className="rounded bg-purple-500 text-white py-2 px-4">Add New</button>
-        </section>
-        <Table data={[{name: "Jump", age:43, school: "MBJ"}, {name : "jdjfv", age: 8, school: "MCA"}]}/>
-      </section>
-    </main>
-  )
-}
+// /* eslint-disable @next/next/no-img-element */
+// export default function Dashboard() {
+//   return (
+//     <main className="flex">
+//       <aside className="h-screen w-1/6 bg-purple-100">
+//         <ul className="flex flex-col text-purple-500">
+//           <li className="p-4"><img src="/images/logo.png"/></li>
+//           <li className="p-4 hover:bg-purple-600 hover:text-purple-100"><a href="">Projects</a></li>
+//           <li className="p-4"><a href="">Investors</a></li>
+//         </ul>
+//       </aside>
+//       <section className="h-screen p-12 w-5/6 flex space-y-3 flex-col">
+//         <section className="flex justify-end">
+//           <button className="rounded bg-purple-500 text-white py-2 px-4">Add New</button>
+//         </section>
+//         <Table data={[{name: "Jump", age:43, school: "MBJ"}, {name : "jdjfv", age: 8, school: "MCA"}]}/>
+//       </section>
+//     </main>
+//   )
+// }
